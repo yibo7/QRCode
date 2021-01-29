626ad1747a5f68df16d5424035d3cf5b11d67155
cqs263 <cqs263@163.com>
2021-01-29 13:14:00 +0800

    初始化项目

---
+++ QRCode/Main.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace QRCode
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.btnMake = new System.Windows.Forms.Button();
+            this.picQR = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picQR)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(25, 36);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(265, 233);
+            this.txtContent.TabIndex = 0;
+            // 
+            // btnMake
+            // 
+            this.btnMake.Location = new System.Drawing.Point(25, 298);
+            this.btnMake.Name = "btnMake";
+            this.btnMake.Size = new System.Drawing.Size(265, 47);
+            this.btnMake.TabIndex = 1;
+            this.btnMake.Text = "生成二维码";
+            this.btnMake.UseVisualStyleBackColor = true;
+            this.btnMake.Click += new System.EventHandler(this.btnMake_Click);
+            // 
+            // picQR
+            // 
+            this.picQR.Location = new System.Drawing.Point(306, 36);
+            this.picQR.Name = "picQR";
+            this.picQR.Size = new System.Drawing.Size(250, 233);
+            this.picQR.TabIndex = 2;
+            this.picQR.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "二维码内容";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(303, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "二维码";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 385);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picQR);
+            this.Controls.Add(this.btnMake);
+            this.Controls.Add(this.txtContent);
+            this.Name = "Main";
+            this.Text = "二维码生成工具";
+            ((System.ComponentModel.ISupportInitialize)(this.picQR)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Button btnMake;
+        private System.Windows.Forms.PictureBox picQR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ QRCode/Main.cs
@@ -0,0 +1,49 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms; 
+
+namespace QRCode
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMake_Click(object sender, EventArgs e)
+        {
+           
+
+            string strContent = txtContent.Text.Trim();
+            if (!string.IsNullOrEmpty(strContent))
+            {
+                string strCode = strContent;
+                QRCodeGenerator qrGenerator = new QRCoder.QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(strCode, QRCodeGenerator.ECCLevel.Q);
+                QRCoder.QRCode qrcode = new QRCoder.QRCode(qrCodeData);
+
+                // qrcode.GetGraphic 方法可参考最下发“补充说明”
+                Bitmap qrCodeImage = qrcode.GetGraphic(5, Color.Black, Color.White, null, 15, 6, false);
+                 
+                 
+                picQR.Image = qrCodeImage;
+                
+            }
+            else
+            {
+                MessageBox.Show("请输入内容再生成!");
+            }
+            
+        }
+
+
+    }
+}
